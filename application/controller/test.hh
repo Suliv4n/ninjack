2d1aa7513e7c3f2f24621d8fad547978090d8fa8
root <sulivan.bochant@gmail.com>
2016-09-12 22:15:53 +0200

    Query builder

--- application/controller/test.hh
+++ application/controller/test.hh
@@ -3,7 +3,9 @@
 use Ninjack\Core\Controller as Controller;
 use Ninjack\Core\Response as Response;
 use Ninjack\Core\Database\Entity as Entity;
+use Ninjack\Core\Enum\DBOperator as DBOperator;
 use Application\Entity\Article as Article;
+use Ninjack\Core\Database\QueryBuilder as QueryBuilder;
 
 class Test extends Controller{
 
@@ -20,8 +22,22 @@ class Test extends Controller{
       var_dump($res);
     }
     */
-    $article = Entity::get(Article::class)[0];
-    $article->save();
+
+
+
+    $conn = $this->get_database("main");
+    if($conn != null){
+      echo "<pre>";
+      var_dump(
+        $conn->query_builder()->update("article", Map{
+            "title" => "Title modified B",
+        })->where("id", 2, DBOperator::NOT_EQUALS)->execute()
+      );
+      echo "</pre>";
+    }
+
+    //$article = Entity::get(Article::class)[0];
+    //$article->save();
     $this->add_to_view("name", $name);
 
   }
